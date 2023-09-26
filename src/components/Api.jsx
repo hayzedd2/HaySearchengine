2481793f8d943fb4aef9470ae2750fd210bce10e
hayzedd2 <azeezalhameen1@gmail.com>
2023-09-26 12:07:01 +0100

    first push

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -41,8 +41,9 @@ const Api = () => {
           <input
             type="text"
             name="street-address"
-            className="block rounded-2xl border-0 py-1.5  flex-1 w-1 px-3 pl-7  text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            className="block rounded-2xl border-0 py-1.5  text-gray-900  ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  flex-1 w-1 px-4 sm:focus sm:border "
             onChange={handleSearch}
+            placeholder="Search for anything"
             value={search}
           />
           <button
@@ -62,14 +63,11 @@ const Api = () => {
         ) : error ? (
           <p>Error : {error.message}</p>
         ) : data && maindata ? (
+          
           <div>
-            {maindata.length === 0 ? null : (
-              <h1 className="search-info">
-                Generated {data.length} results in (
-                {maindata.formattedSearchTime}s)
-              </h1>
-            )}
-
+            {maindata.length === 0 ? null : <h1 className="search-info">Generated {data.length} results in ({maindata.formattedSearchTime}s)</h1>}
+            
+            
             {data.map((data, index) => {
               return (
                 <div key={index}>
